c01d1a2fcecb22e99c25c06eb42c03719c55ed15
taukir <rtaukir26@gmail.com>
2022-10-21 22:49:45 +0530

    pages created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, route, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Post from "./pages/post/Post";
 
 function App() {
   return (
     <>
-    <div>
-      App
-    </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/post" element={<Post />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/post">Post</Link>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
