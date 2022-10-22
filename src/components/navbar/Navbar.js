0860411dbee265ef4312b81b98e58ea7cbf90436
taukir <rtaukir26@gmail.com>
2022-10-22 14:24:14 +0530

    todolist on postPage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-            <Link to="/post">Post</Link>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+      <div className="navbar d-flex justify-content-between">
+        <div className="ms-2">Shop Exp</div>
+        <div className="me-2">
+          <ul className="d-flex">
+            <li className="mx-1">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/post">Post</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Post = () => {
-  return (
-    <div>Post</div>
-  )
-}
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+  const [editId, setEditId] = useState(null);
 
-export default Post
+  //submitting items
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (editId) {
+      let updateItem = items.map((item) => {
+        if (item.id === editId) {
+          return { ...item, input };
+        }
+        return item;
+      });
+      setItems(updateItem);
+      setEditId(null);
+      setInput("");
+    } else {
+      setItems((oldItems) => [
+        ...oldItems,
+        { input, id: new Date().getTime() },
+      ]);
+      setInput("");
+    }
+  };
+
+  //Deleting items
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  //Editing items
+  const editItem = (id, itemName) => {
+    setEditId(id);
+    setInput(itemName);
+  };
+  return (
+    <>
+      <div className="mt-5 d-flex justify-content-around">
+        <div className="form_div">
+          <h5>Add your items</h5>
+          <form onSubmit={handleSubmit} >
+            <input
+              type="text"
+              className="item_input"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            {/* <input type="submit" value="Add" /> */}
+            <button className="addBtn_item" type="submit">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+          </form>
+        </div>
+        <div className="w-50">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="post_item_div d-flex justify-content-between p-1 m-1"
+            >
+              <p>{item.input}</p>
+              <div className="me-2">
+                <i
+                  className=" mx-1 fa-solid fa-pen-to-square btn_edit"
+                  onClick={() => editItem(item.id, item.input)}
+                ></i>
+                <i
+                  className=" mx-1 fa-solid fa-trash btn_delete"
+                  onClick={() => deleteItem(item.id)}
+                ></i>
+              
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Post;
